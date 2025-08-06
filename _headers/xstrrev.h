98af63017dc1f28b420e6da59888b1a5ba629635
CrateOfThunder <xcvi.wgtds.x60@gmail.com>
2025-08-06 04:57:24 -0600

    Update xstrrev.h
    ditto

--- _headers/xstrrev.h
+++ _headers/xstrrev.h
@@ -3,6 +3,10 @@
 /*        xstrrev.h -- agnostic data inversion implementation       */
 /* Ver. 1.00                  08AUG2024              CrateOfThunder */
 /* ---------------------------------------------------------------- */
+#ifndef _XSTRREV_H
+#define _XSTRREV_H
 #include <stddef.h>
 
 void xstrrev(void *v, size_t n);
+
+#endif
