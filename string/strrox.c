fdcb8090a7b08972d5d1f5632fca6fee4bc777cf
CrateOfThunder <xcvi.wgtds.x60@gmail.com>
2025-08-06 04:34:17 -0600

    Update strrox.c
    consistency update

--- string/strrox.c
+++ string/strrox.c
@@ -7,11 +7,12 @@
 
 void strrox(void *v, size_t size, size_t nbits, int shift)
 {
-  uch (*_rox11[2])(uch, uch) = { _rol11, _ror11 },
-      (*_carry[2])(uch) = { _rol, _ror },
-      (*carry)(uch),
-      *p1, *p2, *pp[2], *ptr, carry_bit;
-  void (*_srx[2])(uch *) = { _sll, _srl };
+  unsigned char (*_rox11[2])(unsigned char, unsigned char) =
+    { _rol11, _ror11 },
+    (*_carry[2])(unsigned char) = { _rol, _ror },
+    (*carry)(unsigned char),
+    *p1, *p2, *pp[2], *ptr, carry_bit;
+  void (*_srx[2])(unsigned char *) = { _sll, _srl };
   int xcrement;
 
   if ((size == 0) || (nbits == 0) || ((shift != 1) && (shift != 0)))
@@ -19,7 +20,7 @@ void strrox(void *v, size_t size, size_t nbits, int shift)
 
   xcrement = ~shift + (!shift + 1);
   carry = _carry[shift];
-  p1 = (uch *)v;
+  p1 = (unsigned char *)v;
   p2 = &p1[size - 1];
 
   do {
