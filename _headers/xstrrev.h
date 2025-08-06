1c9848c4161fdcdcb174d6dfef52808dc22f3d5c
CrateOfThunder <xcvi.wgtds.x60@gmail.com>
2025-08-06 04:56:00 -0600

    Create xstrrev.h

---
+++ _headers/xstrrev.h
@@ -0,0 +1,8 @@
+/* ---------------------------------------------------------------- */
+/* C Library Extension for String Reversal                          */
+/*        xstrrev.h -- agnostic data inversion implementation       */
+/* Ver. 1.00                  08AUG2024              CrateOfThunder */
+/* ---------------------------------------------------------------- */
+#include <stddef.h>
+
+void xstrrev(void *v, size_t n);
